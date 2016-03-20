1dce54fb49d818666798889babda09408d072614
Brian King <brianjosephking@gmail.com>
2016-03-19 23:29:16 -0500

    possibly display_github was in wrong spot

--- source/conf.py
+++ source/conf.py
@@ -138,7 +138,8 @@ todo_include_todos = False
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'display_version': False}
+    'display_version': False
+    'display_github': False }
 
 
 # Add any paths that contain custom themes here, relative to this directory.
