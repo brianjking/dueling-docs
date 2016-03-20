d522d27f6f1ed74fe022a2f300948b28a169f515
Brian King <brianjosephking@gmail.com>
2016-03-19 23:26:51 -0500

    perhaps display_github fixes

--- source/conf.py
+++ source/conf.py
@@ -88,6 +88,8 @@ release = u'0.5'
 # Usually you set "language" from the command line for these cases.
 language = None
 
+display_github = False
+
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #today = ''
