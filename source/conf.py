09b0b8415700e7ad5e57220f3bef33b9e6e72f16
Brian King <brianjosephking@gmail.com>
2016-03-20 00:05:00 -0500

    nope

--- source/conf.py
+++ source/conf.py
@@ -143,8 +143,8 @@ todo_include_todos = False
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'display_github': False
-    }
+
+}
 
 
 # Add any paths that contain custom themes here, relative to this directory.
