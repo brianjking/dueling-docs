92d01fa6ade931e31782c8df85de32678da9cbc1
Brian King <brianjosephking@gmail.com>
2016-03-19 19:37:18 -0500

    test

--- source/conf.py
+++ source/conf.py
@@ -135,7 +135,8 @@ todo_include_todos = False
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {
+#html_theme_options =
+{
     'display_version': False
     }
 
