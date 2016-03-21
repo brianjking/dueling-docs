a72301bd01ebb68cf67c09a933fb233b5db06bb3
Brian King <brianjosephking@gmail.com>
2016-03-21 13:46:15 -0500

    trying to implement issue collector

---
+++ source/_static/jira.js
@@ -0,0 +1,7 @@
+// Requires jQuery!
+jQuery.ajax({
+            url: "http://104.197.132.121/s/83ddc972c3081d22fffbf8446fbc64db-T/en_US-kq69xc/71006/b6b48b2829824b869586ac216d119363/2.0.11/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=680c22f9",
+        type: "get",
+    cache: true,
+    dataType: "script"
+});
