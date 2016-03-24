2f6f8806d1d2eea9f0b012586707ebc026cb0d2b
Brian King <brianjosephking@gmail.com>
2016-03-24 10:01:21 -0500

    Re-adding Jira issue collector functionality, now in production configuration

--- source/_static/jira.js
+++ source/_static/jira.js
@@ -1,7 +1,7 @@
 // Requires jQuery!
 jQuery.ajax({
-            url: "http://104.197.132.121/s/83ddc972c3081d22fffbf8446fbc64db-T/en_US-kq69xc/71006/b6b48b2829824b869586ac216d119363/2.0.11/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=680c22f9",
-        type: "get",
+    url: "https://duelingdogs.atlassian.net/s/f854cdb069361040ccdd74988bb7959f-T/-e9m5hs/72002/b6b48b2829824b869586ac216d119363/2.0.13/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=9d0c7d4b",
+    type: "get",
     cache: true,
     dataType: "script"
 });
